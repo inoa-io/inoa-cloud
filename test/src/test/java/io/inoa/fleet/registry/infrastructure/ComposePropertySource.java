183e20c4845696225a36ff8eb0604bf9b5177f9c
Stephan Schnabel <spampot+github@posteo.eu>
2021-08-31 16:05:48 +0200

    Use dns for compose.

--- test/src/test/java/io/inoa/fleet/registry/infrastructure/ComposePropertySource.java
+++ test/src/test/java/io/inoa/fleet/registry/infrastructure/ComposePropertySource.java
@@ -1,5 +1,6 @@
 package io.inoa.fleet.registry.infrastructure;
 
+import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Paths;
 import java.time.Duration;
@@ -16,6 +17,7 @@ import io.micronaut.context.env.MapPropertySource;
 import io.micronaut.context.env.PropertySource;
 import io.micronaut.context.env.PropertySourceLoader;
 import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.core.io.socket.SocketUtils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -26,6 +28,11 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class ComposePropertySource implements PropertySourceLoader {
+
+	private static final File COMPOSE_FILE = Paths.get("target/compose/docker-compose.yaml").toFile();
+	private static final String CONTAINER_POSTGRES = "postgres";
+	private static final String CONTAINER_KEYCLOAK = "keycloak";
+	private static final String CONTAINER_SERVICE = "gateway-registry-service";
 
 	private static Map<String, Object> cache;
 
@@ -39,47 +46,36 @@ public class ComposePropertySource implements PropertySourceLoader {
 		return Optional.empty();
 	}
 
+	@SuppressWarnings("resource")
 	@Override
 	public Optional<PropertySource> loadEnv(String resource, ResourceLoader loader, ActiveEnvironment environment) {
 		if (cache == null) {
-			cache = handleCompose();
+			if (SocketUtils.isTcpPortAvailable(5380)) {
+				log.info("Start docker-compose containers.");
+				var waitForHealthcheck = Wait.forHealthcheck().withStartupTimeout(Duration.ofMinutes(2));
+				var container = new DockerComposeContainer<>(COMPOSE_FILE)
+						.withServices(CONTAINER_SERVICE, CONTAINER_KEYCLOAK, CONTAINER_POSTGRES)
+						.withExposedService(CONTAINER_SERVICE, 8080, waitForHealthcheck)
+						.withExposedService(CONTAINER_SERVICE, 8090, waitForHealthcheck)
+						.withExposedService(CONTAINER_KEYCLOAK, 8080, waitForHealthcheck)
+						.withLogConsumer(CONTAINER_SERVICE, new Slf4jLogConsumer(log).withPrefix("service"))
+						.withLogConsumer(CONTAINER_KEYCLOAK, new Slf4jLogConsumer(log).withPrefix(CONTAINER_KEYCLOAK))
+						.withRemoveImages(RemoveImages.ALL)
+						.withLocalCompose(false);
+				container.start();
+				cache = Map.of(
+						"test.service.8080", "localhost:" + container.getServicePort(CONTAINER_SERVICE, 8080),
+						"test.service.8090", "localhost:" + container.getServicePort(CONTAINER_SERVICE, 8090),
+						"test.keycloak.8080", "localhost:" + container.getServicePort(CONTAINER_KEYCLOAK, 8080));
+			} else {
+				log.info("Use existing containers and skip compose start.");
+				cache = Map.of(
+						"test.service.8080", CONTAINER_SERVICE + ":" + 8080,
+						"test.service.8090", CONTAINER_SERVICE + ":" + 8090,
+						"test.keycloak.8080", CONTAINER_KEYCLOAK + ":" + 8080);
+			}
+			log.info("Use properties: {}", cache);
 		}
 		return Optional.of(new MapPropertySource("testcontainers", cache));
-	}
-
-	@SuppressWarnings("resource")
-	private Map<String, Object> handleCompose() {
-
-		// check if compose should be used
-
-		var skipCompose = Boolean.parseBoolean(System.getProperty("skipCompose", "false"));
-		if (skipCompose) {
-			log.info("Use existing containers and skip compose start.");
-			return Map.of();
-		}
-
-		// start container
-
-		var wait = Wait.forHealthcheck().withStartupTimeout(Duration.ofMinutes(5));
-		var container = new DockerComposeContainer<>(Paths.get("target/compose/docker-compose.yaml").toFile())
-				.withExposedService("gateway-registry-service", 8080, wait)
-				.withExposedService("gateway-registry-service", 8090, wait)
-				.withExposedService("keycloak", 9000, wait)
-				.withLogConsumer("gateway-registry-service", new Slf4jLogConsumer(log).withPrefix("service"))
-				.withLogConsumer("keycloak", new Slf4jLogConsumer(log).withPrefix("keycloak"))
-				.withLogConsumer("postgres", new Slf4jLogConsumer(log).withPrefix("postgres"))
-				.withRemoveImages(RemoveImages.ALL)
-				.withLocalCompose(false);
-		container.start();
-
-		// publish mapped ports
-
-		var properties = Map.<String, Object>of(
-				"docker.gateway-registry-service.8080", container.getServicePort("gateway-registry-service", 8080),
-				"docker.gateway-registry-service.8090", container.getServicePort("gateway-registry-service", 8090),
-				"docker.keycloak.9000", container.getServicePort("keycloak", 9000));
-		log.info("Started compose: {}", properties);
-
-		return properties;
 	}
 }
