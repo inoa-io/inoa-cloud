a9edd43263ca3dd47fbaa979cf12780e91b3d034
Stephan Schnabel <spampot+github@posteo.eu>
2021-10-29 14:53:56 +0200

    Remove kafka-backup.

--- test/src/test/java/io/inoa/fleet/registry/test/MonitoringClient.java
+++ test/src/test/java/io/inoa/fleet/registry/test/MonitoringClient.java
@@ -9,7 +9,6 @@ import javax.inject.Singleton;
 
 import org.awaitility.Awaitility;
 
-import io.inoa.fleet.registry.test.prometheus.BackupPrometheusClient;
 import io.inoa.fleet.registry.test.prometheus.ExporterPrometheusClient;
 import io.inoa.fleet.registry.test.prometheus.TranslatorPrometheusClient;
 import lombok.RequiredArgsConstructor;
@@ -19,29 +18,13 @@ import lombok.RequiredArgsConstructor;
 public class MonitoringClient {
 
 	private final Map<String, Long> before = new HashMap<>();
-	private final BackupPrometheusClient backup;
 	private final TranslatorPrometheusClient translator;
 	private final ExporterPrometheusClient exporter;
 
 	public void reset() {
 		before.clear();
-		before.put("backup.scrapMessages", backup.scrapMessages());
 		before.put("translator.scrapMessagesSuccess", translator.scrapMessagesSuccess());
 		before.put("exporter.scrapKafkaRecords", exporter.scrapKafkaRecords());
-	}
-
-	// backup
-
-	public long backupMessagesBefore() {
-		return before.get("backup.scrapMessages");
-	}
-
-	public long backupMessagesNow() {
-		return backup.scrapMessages();
-	}
-
-	public void awaitbackupMessages(String message, int amount) {
-		await(message, () -> backupMessagesNow() == backupMessagesBefore() + amount);
 	}
 
 	// translator
