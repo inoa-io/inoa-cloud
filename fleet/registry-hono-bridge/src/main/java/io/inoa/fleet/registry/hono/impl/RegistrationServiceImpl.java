e913f20f17c7080ee446ead72f5d0818d16605eb
Rico Pahlisch <rico.pahlisch@grayc.de>
2022-04-21 14:09:28 +0200

    fix hono defaults (#141)

--- fleet/registry-hono-bridge/src/main/java/io/inoa/fleet/registry/hono/impl/RegistrationServiceImpl.java
+++ fleet/registry-hono-bridge/src/main/java/io/inoa/fleet/registry/hono/impl/RegistrationServiceImpl.java
@@ -3,11 +3,9 @@ package io.inoa.fleet.registry.hono.impl;
 import java.net.HttpURLConnection;
 import java.util.Map;
 
-import org.eclipse.hono.service.management.device.Device;
 import org.eclipse.hono.service.registration.RegistrationService;
 import org.eclipse.hono.util.RegistrationConstants;
 import org.eclipse.hono.util.RegistrationResult;
-import org.eclipse.hono.util.RequestResponseApiConstants;
 import org.springframework.stereotype.Service;
 
 import io.inoa.fleet.registry.hono.rest.RegistryClient;
@@ -29,13 +27,10 @@ public class RegistrationServiceImpl implements RegistrationService {
 	public Future<RegistrationResult> assertRegistration(String tenantId, String deviceId) {
 		log.info("assertRegistration {}@{}", deviceId, tenantId);
 		return Future.succeededFuture(registryClient.findGateway(tenantId, deviceId)
-				.map(device -> new JsonObject()
-						.put(RequestResponseApiConstants.FIELD_PAYLOAD_DEVICE_ID, deviceId)
-						.put(RegistrationConstants.FIELD_DATA, JsonObject.mapFrom(new Device()
-								.setEnabled(device.getEnabled())
-								// TODO move the defaults to the database
-								// it is only a workaround for now
-								.setDefaults(Map.of("gatewayName", device.getName(), "gatewayType", "satellite")))))
+				.map(device -> new JsonObject().put(RegistrationConstants.FIELD_PAYLOAD_DEVICE_ID, deviceId)
+						.put(RegistrationConstants.FIELD_ENABLED, device.getEnabled())
+						.put(RegistrationConstants.FIELD_PAYLOAD_DEFAULTS,
+								Map.of("gatewayName", device.getName(), "gatewayType", "satellite")))
 				.map(json -> RegistrationResult.from(HttpURLConnection.HTTP_OK, json, properties.getGatewayCache()))
 				.orElseGet(() -> RegistrationResult.from(HttpURLConnection.HTTP_NOT_FOUND)));
 	}
