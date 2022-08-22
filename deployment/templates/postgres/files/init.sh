828e2c3e2ea8328258c577561b7cea5e7c2f3dc8
Stephan Schnabel <stephan.schnabel@posteo.de>
2022-08-22 11:02:57 +0200

    INOA-270 Cleanup tests, start k3s in test

---
+++ deployment/templates/postgres/files/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+
+    CREATE DATABASE "keycloak";
+    CREATE USER "keycloak" WITH ENCRYPTED PASSWORD 'changeMe';
+    GRANT ALL PRIVILEGES ON DATABASE "keycloak" TO "keycloak";
+
+    CREATE DATABASE "grafana";
+    CREATE USER "grafana" WITH ENCRYPTED PASSWORD 'changeMe';
+    GRANT ALL PRIVILEGES ON DATABASE "grafana" TO "grafana";
+
+    CREATE DATABASE "registry";
+    CREATE USER "registry" WITH ENCRYPTED PASSWORD 'changeMe';
+    GRANT ALL PRIVILEGES ON DATABASE "registry" TO "registry";
+
+EOSQL
