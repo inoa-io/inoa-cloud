ed4635dadbf09989212919b3ea7fd27a97af076b
Fabian Schlegel <fabian.schlegel@grayc.de>
2022-11-07 15:59:48 +0100

    INOA-314 Added simple converter without zero glitch handling

---
+++ measurement/translator/src/test/java/io/inoa/measurement/translator/converter/common/S0ConverterTest.java
@@ -0,0 +1,63 @@
+package io.inoa.measurement.translator.converter.common;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+
+import io.inoa.fleet.telemetry.TelemetryRawVO;
+import io.inoa.measurement.translator.converter.AbstractConverterTest;
+import io.inoa.measurement.translator.converter.LogSink;
+import jakarta.inject.Inject;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.inoa.measurement.translator.converter.LogSink.assertMessage;
+
+public class S0ConverterTest extends AbstractConverterTest {
+
+	@Inject
+	S0Converter converter;
+
+	@BeforeEach
+	void setup() {
+		LogSink.setUp(S0Converter.class);
+	}
+
+	@DisplayName("success: with modifier")
+	@Test
+	void successWithModifier() {
+		var telemetry = telemetry("1234567");
+		assertSingleValue(1234.567, converter, telemetry, "s0", "gas");
+		assertMessage(S0Converter.class, "S0 count has value: 1234567");
+	}
+
+	@DisplayName("fail: count empty")
+	@Test
+	void failByteCountEmpty() {
+		assertEmpty(converter, telemetry(""), "s0", "gas");
+		assertMessage(S0Converter.class, "Retrieved invalid S0 message (empty)");
+	}
+
+	@DisplayName("fail: count NaN")
+	@Test
+	void failByteCountNaN() {
+		assertEmpty(converter, telemetry("DreiZehnKommaZweiEins"), "s0", "gas");
+		assertMessage(S0Converter.class, "Retrieved invalid S0 message (not a number)");
+	}
+
+	@DisplayName("fail: count negative")
+	@Test
+	void failByteCountNegative() {
+		assertEmpty(converter, telemetry("-42"), "s0", "gas");
+		assertMessage(S0Converter.class, "Retrieved invalid S0 message (negative value)");
+	}
+
+	@SneakyThrows
+	private TelemetryRawVO telemetry(String value) {
+		return new TelemetryRawVO()
+			.urn("urn:s0:0:gas")
+			.timestamp(Instant.now().toEpochMilli())
+			.value(value.getBytes(StandardCharsets.UTF_8));
+	}
+}
