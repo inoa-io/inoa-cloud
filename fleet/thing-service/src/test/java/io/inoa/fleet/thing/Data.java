e032825514fd263c30fceff0bcc64c458519cc8d
Rico Pahlisch <rico.pahlisch@grayc.de>
2022-12-20 09:16:06 +0100

    add test for gateway command call (#270)

--- fleet/thing-service/src/test/java/io/inoa/fleet/thing/Data.java
+++ fleet/thing-service/src/test/java/io/inoa/fleet/thing/Data.java
@@ -2,7 +2,9 @@ package io.inoa.fleet.thing;
 
 import javax.transaction.Transactional;
 
+import io.inoa.fleet.thing.domain.Thing;
 import io.inoa.fleet.thing.domain.ThingRepository;
+import io.inoa.fleet.thing.domain.ThingType;
 import io.inoa.fleet.thing.domain.ThingTypeRepository;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -22,5 +24,22 @@ public class Data {
 		// first delete all things as we have an FK on thing type but no cascade delete
 		thingRepository.deleteAll();
 		thingTypeRepository.deleteAll();
+	}
+
+	public ThingType createThingType(String name, String thingTypeReference) {
+		ThingType thingType = new ThingType();
+		thingType.setName(name);
+		thingType.setThingTypeReference(thingTypeReference);
+		thingType.setThingTypeId("");
+		return thingTypeRepository.save(thingType);
+	}
+	public Thing createThing(String gatewayId, String tenantId, ThingType thingType) {
+		Thing thing = new Thing();
+		thing.setThingId("");
+		thing.setTenantId(tenantId);
+		thing.setName("");
+		thing.setThingType(thingType);
+		thing.setGatewayId(gatewayId);
+		return thingRepository.save(thing);
 	}
 }
