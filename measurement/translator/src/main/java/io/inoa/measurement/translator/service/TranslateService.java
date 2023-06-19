86e42f7fe08fa116b79f85c48afcbd8ed4ae2a00
Stephan Schnabel <stephan.schnabel@posteo.de>
2023-06-19 14:38:33 +0200

    INOA-197 Add check for watthour in translator

--- measurement/translator/src/main/java/io/inoa/measurement/translator/service/TranslateService.java
+++ measurement/translator/src/main/java/io/inoa/measurement/translator/service/TranslateService.java
@@ -1,5 +1,6 @@
 package io.inoa.measurement.translator.service;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Base64;
 import java.util.Comparator;
@@ -7,15 +8,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.slf4j.MDC;
 
 import io.inoa.fleet.telemetry.TelemetryRawVO;
 import io.inoa.measurement.telemetry.TelemetryVO;
 import io.inoa.measurement.translator.converter.Converter;
+import io.micronaut.cache.CacheManager;
+import io.micronaut.cache.SyncCache;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.core.order.Ordered;
 import jakarta.inject.Singleton;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -23,7 +27,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Singleton
 @Slf4j
-@RequiredArgsConstructor
 public class TranslateService {
 
 	private static Pattern URN_PATTERN = Pattern.compile("urn"
@@ -33,6 +36,19 @@ public class TranslateService {
 
 	private final Set<Converter> converters;
 	private final TranslateMetrics metrics;
+	private final SyncCache<?> cache;
+	private final int increaseThreshold;
+
+	public TranslateService(
+			Set<Converter> converters,
+			TranslateMetrics metrics,
+			CacheManager<?> cacheManager,
+			@Value("${inoa.measurement.translator.increaseThreshold:100000}") int increaseThreshold) {
+		this.converters = converters;
+		this.metrics = metrics;
+		this.cache = cacheManager.getCache("watthour");
+		this.increaseThreshold = increaseThreshold;
+	}
 
 	public List<TelemetryVO> translate(String tenantId, String gatewayId, TelemetryRawVO raw) {
 
@@ -66,12 +82,14 @@ public class TranslateService {
 
 		var messages = converter.get().convert(raw, deviceType, sensor)
 				.peek(target -> fillMessage(raw, target, tenantId, gatewayId, deviceType, deviceId, sensor))
-				.toList();
+				.collect(Collectors.toList());
 		if (messages.isEmpty()) {
 			log.warn("Failed on value: {}", Base64.getEncoder().encode(raw.getValue()));
 			metrics.counterFailValue(tenantId, deviceType, sensor).increment();
 			return List.of();
 		}
+
+		messages.removeIf(this::isInvalid);
 
 		return messages;
 	}
@@ -110,5 +128,55 @@ public class TranslateService {
 			}
 			raw.getExt().forEach(telemetry.getExt()::putIfAbsent);
 		}
+	}
+
+	private boolean isInvalid(TelemetryVO vo) {
+
+		if (vo.getExt() == null || !"watthour".equals(vo.getExt().get("unit"))) {
+			return false;
+		}
+
+		var valid = true;
+		var urn = vo.getUrn();
+		var old = cache.get(urn, TelemetryVO.class).orElse(null);
+
+		if (old == null) {
+			log.trace("URN {} had no cached value", urn);
+		} else {
+
+			var oldTime = old.getTimestamp();
+			var newTime = vo.getTimestamp();
+			var diffTime = Duration.between(oldTime, newTime);
+			var newValue = vo.getValue();
+			var oldValue = old.getValue();
+			var diffValue = newValue - oldValue;
+
+			if (diffTime.isNegative()) {
+				log.warn("URN {} got out of order value (old {}, new {}), skip validation.", urn, oldTime, newTime);
+				return true;
+			}
+
+			if (diffValue < 0) {
+				log.warn("URN {} had wh decreased from {} to {} Wh (diff {} Wh).",
+						urn, oldValue, newValue, diffValue);
+				valid = false;
+			} else {
+				// normalized to 1h
+				var increase = diffValue / (diffTime.getSeconds() / 3_600D);
+				if (increase > increaseThreshold) {
+					log.warn("URN {} increased by {} Wh ({} Wh in {}), skip value bigger than thresold {} Wh.",
+							urn, increase, diffValue, diffTime, increaseThreshold);
+					valid = false;
+				} else {
+					log.trace("URN {} increased by {} Wh ({} Wh in {}).", urn, increase, diffValue, diffTime);
+				}
+			}
+		}
+
+		if (valid) {
+			cache.put(urn, vo);
+		}
+
+		return !valid;
 	}
 }
--- measurement/translator/src/test/java/io/inoa/measurement/translator/service/TranslateServiceTest.java
+++ measurement/translator/src/test/java/io/inoa/measurement/translator/service/TranslateServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,104 @@ import org.junit.jupiter.api.Test;
 import io.inoa.fleet.telemetry.TelemetryRawVO;
 import io.inoa.measurement.telemetry.TelemetryVO;
 import io.inoa.measurement.translator.AbstractTest;
+import io.micronaut.cache.CacheManager;
 import jakarta.inject.Inject;
 
 public class TranslateServiceTest extends AbstractTest {
 
 	@Inject
 	TranslateService service;
+
+	@DisplayName("Watthour")
+	@Test
+	void wattHour(CacheManager<?> cacheManager) {
+
+		var cache = cacheManager.getCache("watthour");
+		cache.invalidateAll();
+
+		var tenantId = "inoa";
+		var gatewayId = "GW-0001";
+		var urn = "urn:example:0815:watthour";
+		var now = Instant.ofEpochSecond(0);
+
+		// with empty cache
+
+		var init = new TelemetryVO()
+				.tenantId(tenantId)
+				.gatewayId(gatewayId)
+				.urn(urn)
+				.deviceType("example")
+				.deviceId("0815")
+				.sensor("watthour")
+				.timestamp(now.plusSeconds(60))
+				.value(100D)
+				.ext(Map.of("unit", "watthour"));
+		assertEquals(List.of(init), service.translate(tenantId, gatewayId, new TelemetryRawVO()
+				.urn(urn)
+				.timestamp(now.plusSeconds(60).toEpochMilli())
+				.value("100".getBytes())), "init inoa vo");
+		assertEquals(init, cache.get(urn, TelemetryVO.class).orElse(null), "init cache vo");
+
+		// with filled cache and valid value
+
+		var valid = new TelemetryVO()
+				.tenantId(tenantId)
+				.gatewayId(gatewayId)
+				.urn(urn)
+				.deviceType("example")
+				.deviceId("0815")
+				.sensor("watthour")
+				.timestamp(now.plusSeconds(120))
+				.value(200D)
+				.ext(Map.of("unit", "watthour"));
+		assertEquals(List.of(valid), service.translate(tenantId, gatewayId, new TelemetryRawVO()
+				.urn(urn)
+				.timestamp(now.plusSeconds(120).toEpochMilli())
+				.value("200".getBytes())), "valid inoa vo");
+		assertEquals(valid, cache.get(urn, TelemetryVO.class).orElse(null), "valid cache vo");
+
+		// with filled cache and outdated value
+
+		assertEquals(List.of(), service.translate(tenantId, gatewayId, new TelemetryRawVO()
+				.urn(urn)
+				.timestamp(now.plusSeconds(110).toEpochMilli())
+				.value("190".getBytes())), "outdated inoa vo");
+		assertEquals(valid, cache.get(urn, TelemetryVO.class).orElse(null), "outdated cache vo");
+
+		// with filled cache and outdated value
+
+		assertEquals(List.of(), service.translate(tenantId, gatewayId, new TelemetryRawVO()
+				.urn(urn)
+				.timestamp(now.plusSeconds(130).toEpochMilli())
+				.value("190".getBytes())), "negative inoa vo");
+		assertEquals(valid, cache.get(urn, TelemetryVO.class).orElse(null), "negative cache vo");
+
+		// with filled cache and very large plus value
+
+		assertEquals(List.of(), service.translate(tenantId, gatewayId, new TelemetryRawVO()
+				.urn(urn)
+				.timestamp(now.plusSeconds(3720).toEpochMilli())
+				.value("100201".getBytes())), "threshold inoa vo");
+		assertEquals(valid, cache.get(urn, TelemetryVO.class).orElse(null), "threshold cache vo");
+
+		// with filled cache and valid value again
+
+		var valid1 = new TelemetryVO()
+				.tenantId(tenantId)
+				.gatewayId(gatewayId)
+				.urn(urn)
+				.deviceType("example")
+				.deviceId("0815")
+				.sensor("watthour")
+				.timestamp(now.plusSeconds(180))
+				.value(300D)
+				.ext(Map.of("unit", "watthour"));
+		assertEquals(List.of(valid1), service.translate(tenantId, gatewayId, new TelemetryRawVO()
+				.urn(urn)
+				.timestamp(now.plusSeconds(180).toEpochMilli())
+				.value("300".getBytes())), "valid inoa vo");
+		assertEquals(valid1, cache.get(urn, TelemetryVO.class).orElse(null), "valid cache vo");
+	}
 
 	@DisplayName("Example - base64 to single value")
 	@Test
