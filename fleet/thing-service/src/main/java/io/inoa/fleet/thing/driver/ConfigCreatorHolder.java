ecd134309f7731ee1a1c6aa485d477864cc1e500
fabian-schlegel <fabian.schlegel@grayc.de>
2023-09-28 13:59:06 +0200

    Add dvze serial to timerseries (#366)
    * Introduce dedicated thing type for DVZE meters
    
    * Added new thing type to translator config

--- fleet/thing-service/src/main/java/io/inoa/fleet/thing/driver/ConfigCreatorHolder.java
+++ fleet/thing-service/src/main/java/io/inoa/fleet/thing/driver/ConfigCreatorHolder.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.inoa.fleet.thing.domain.ThingType;
+import io.inoa.fleet.thing.driver.modbus.DVZEDvModbusIRBuilder;
 import io.inoa.fleet.thing.driver.modbus.DvModbusIRBuilder;
 import io.inoa.fleet.thing.driver.modbus.ModbusDVH4013Builder;
 import io.inoa.fleet.thing.driver.modbus.ModbusMDVH4006Builder;
@@ -19,6 +20,7 @@ public class ConfigCreatorHolder {
 	private final Map<String, ConfigCreator> creators = new HashMap<>();
 
 	public ConfigCreatorHolder(ObjectMapper objectMapper) {
+		creators.put("dvmodbusir-dvze", new DVZEDvModbusIRBuilder(objectMapper));
 		creators.put("dvmodbusir", new DvModbusIRBuilder(objectMapper));
 		creators.put("dvh4013", new ModbusDVH4013Builder(objectMapper));
 		creators.put("mdvh4006", new ModbusMDVH4006Builder(objectMapper));
