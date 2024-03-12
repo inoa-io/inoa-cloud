1ffcd80700f7f04aeb55f8e4844443d1238f657b
Stephan Schnabel <stephan.schnabel@posteo.de>
2024-03-12 14:35:30 +0000

    Add chrome for tests (#87)
    Reviewed-on: https://git.grayc.io/inoa/cloud/pulls/87
    Reviewed-by: Ronny Schlegel <ronny.schlegel@noreply.git.grayc.io>
    Co-authored-by: Stephan Schnabel <stephan.schnabel@posteo.de>
    Co-committed-by: Stephan Schnabel <stephan.schnabel@posteo.de>

---
+++ app/karma.conf.ts
@@ -0,0 +1,20 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = (config) => {
+	config.set({
+		frameworks: ["jasmine", "@angular-devkit/build-angular"],
+		plugins: [
+			require("karma-jasmine"),
+			require("karma-chrome-launcher"),
+			require("@angular-devkit/build-angular/plugins/karma")
+		],
+		browsers: ["ChromeHeadlessCI"],
+		customLaunchers: {
+			ChromeHeadlessCI: {
+				base: "ChromeHeadless",
+				flags: ["--no-sandbox"]
+			}
+		}
+	});
+};
