b8b5fb10eefab74de4ffa4bf22433b1b15e47055
Jörn Schwarze <joern.schwarze@grayc.de>
2024-10-17 10:50:20 +0000

    Fixed locally running yarn instance with environment file. (#148)
    Reviewed-on: https://git.grayc.io/inoa/cloud/pulls/148
    Reviewed-by: Ronny Schlegel <ronny.schlegel@noreply.git.grayc.io>

--- app/src/proxy.conf.mjs
+++ app/src/proxy.conf.mjs
@@ -1,9 +1,11 @@
+const INOA_DOMAIN = process.env["INOA_DOMAIN"] ? process.env["INOA_DOMAIN"] : "127.0.0.1.nip.io";
+
 export default [
   {
     context: [
       '/api',
     ],
-    "target": "http://inoa.default:8080",
+    "target": "http://inoa." + INOA_DOMAIN + ":8080",
     "secure": false,
     "changeOrigin": true,
     "logLevel": "debug",
