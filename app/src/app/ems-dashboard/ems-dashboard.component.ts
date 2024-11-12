7df36bf636093d00f77ea16c4dedc0a63734413a
Zamuroy <Zamuroy@gmail.com>
2024-11-12 15:17:57 +0100

    grafana dashboard link now derived from the current url

--- app/src/app/ems-dashboard/ems-dashboard.component.ts
+++ app/src/app/ems-dashboard/ems-dashboard.component.ts
@@ -6,5 +6,16 @@ import { Component } from "@angular/core";
   styleUrl: "./ems-dashboard.component.css"
 })
 export class EmsDashboardComponent {
-  
+  getGrafanaDashboardUrl(): string {
+
+    let currentUrl = window.location.href;
+    let newUrl = currentUrl.replace('inoa', 'grafana');
+    let baseUrl = newUrl.split('/')[0] + '//' + newUrl.split('/')[2];
+
+    const dashboardUrl = baseUrl + "/d/aed25ae6-bcfd-4d5b-9571-adad2755e486/ems-power-data?orgId=1&refresh=15m&theme=light&kiosk";
+
+    console.log("This should be the Grafana Dashboard Url: " + dashboardUrl);
+
+    return dashboardUrl;
+  }
 }
