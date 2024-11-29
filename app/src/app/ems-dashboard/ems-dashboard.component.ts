6339915a88d3c4453107358f08a93804d1271f44
Zamuroy <Zamuroy@gmail.com>
2024-11-29 14:10:20 +0100

    linted

--- app/src/app/ems-dashboard/ems-dashboard.component.ts
+++ app/src/app/ems-dashboard/ems-dashboard.component.ts
@@ -8,9 +8,9 @@ import { Component } from "@angular/core";
 export class EmsDashboardComponent {
   getGrafanaDashboardUrl(): string {
 
-    let currentUrl = window.location.href;
-    let newUrl = currentUrl.replace('inoa', 'grafana');
-    let baseUrl = newUrl.split('/')[0] + '//' + newUrl.split('/')[2];
+    const currentUrl = window.location.href;
+    const newUrl = currentUrl.replace("inoa", "grafana");
+    const baseUrl = newUrl.split("/")[0] + "//" + newUrl.split("/")[2];
 
     const dashboardUrl = baseUrl + "/d/aed25ae6-bcfd-4d5b-9571-adad2755e486/ems-power-data?orgId=1&refresh=15m&theme=light&kiosk";
 
