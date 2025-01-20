283ec351922ac2567008230bda3216596f9e517c
ronny.schlegel <ronny.schlegel@grayc.de>
2025-01-20 11:21:41 +0100

    added time window parameter directly to the grafana dashboard url

--- app/src/app/ems-dashboard/ems-dashboard.component.ts
+++ app/src/app/ems-dashboard/ems-dashboard.component.ts
@@ -19,7 +19,7 @@ export class EmsDashboardComponent {
     const newUrl = currentUrl.replace("inoa", "grafana");
     const baseUrl = newUrl.split("/")[0] + "//" + newUrl.split("/")[2];
 
-    const dashboardUrl = baseUrl + "/d/aed25ae6-bcfd-4d5b-9571-adad2755e486/ems-power-data?orgId=1&refresh=15m&theme=light&kiosk";
+    const dashboardUrl = baseUrl + "/d/aed25ae6-bcfd-4d5b-9571-adad2755e486/ems-power-data?orgId=1&refresh=15m&theme=light&kiosk&from=now-3h&to=now";
 
     console.log("This should be the Grafana Dashboard Url: " + dashboardUrl);
 
