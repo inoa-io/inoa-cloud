4be119f10e906474db0e77fbd3cebbf25064a3ec
Stephan Schnabel <stephan@schnabel.org>
2025-06-26 21:15:02 +0200

    chore(deprecation): refactor to avoid deprecated netty function

--- service/src/main/java/io/inoa/fleet/mqtt/MqttFactory.java
+++ service/src/main/java/io/inoa/fleet/mqtt/MqttFactory.java
@@ -1,14 +1,22 @@
 package io.inoa.fleet.mqtt;
 
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.KeyPairGenerator;
 import java.security.Security;
-import java.security.cert.CertificateException;
+import java.util.Date;
 import java.util.Properties;
 
 import javax.net.ssl.SSLException;
+import javax.security.auth.x500.X500Principal;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Singleton;
 
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.OperatorException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +27,6 @@ import io.moquette.broker.config.MemoryConfig;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslProtocols;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
 
 /**
  * Factory for mqtt broker.
@@ -44,8 +51,7 @@ public class MqttFactory {
 		config.setProperty(IConfig.ALLOW_ANONYMOUS_PROPERTY_NAME, String.valueOf(false));
 		config.setProperty(IConfig.HOST_PROPERTY_NAME, properties.getHost());
 		config.setProperty(IConfig.PORT_PROPERTY_NAME, String.valueOf(properties.getPort()));
-		config.setProperty(
-				IConfig.SSL_PORT_PROPERTY_NAME, String.valueOf(properties.getTls().getPort()));
+		config.setProperty(IConfig.SSL_PORT_PROPERTY_NAME, String.valueOf(properties.getTls().getPort()));
 		config.setProperty(IConfig.PERSISTENCE_ENABLED_PROPERTY_NAME, String.valueOf(false));
 		// Immediate flush to avoid timing issues in tests or dependant clients
 		config.setProperty(IConfig.BUFFER_FLUSH_MS_PROPERTY_NAME, String.valueOf(0));
@@ -54,12 +60,18 @@ public class MqttFactory {
 	}
 
 	@Singleton
-	SslContext sslContext(MqttProperties properties) throws SSLException, CertificateException {
+	SslContext sslContext(MqttProperties properties) throws SSLException, GeneralSecurityException, OperatorException {
 		SslContextBuilder builder;
 		var tls = properties.getTls();
 		if (tls.isGenerateKey()) {
-			var cert = new SelfSignedCertificate();
-			builder = SslContextBuilder.forServer(cert.key(), cert.cert());
+			var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(1024);
+			var keyPair = keyPairGenerator.generateKeyPair();
+			var subject = new X500Principal("CN=nope");
+			var cert = new JcaX509CertificateConverter().getCertificate(new JcaX509v3CertificateBuilder(subject,
+					BigInteger.ONE, new Date(0), new Date(Integer.MAX_VALUE), subject, keyPair.getPublic())
+							.build(new JcaContentSignerBuilder("SHA256withRSA").build(keyPair.getPrivate())));
+			builder = SslContextBuilder.forServer(keyPair.getPrivate(), cert);
 		} else {
 			builder = SslContextBuilder.forServer(tls.getCert().toFile(), tls.getKey().toFile());
 		}
