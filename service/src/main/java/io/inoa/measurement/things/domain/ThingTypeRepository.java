62573f0cc6cb247791144bc62212392ab8a086e9
fabian-schlegel <fabian.schlegel@grayc.de>
2025-07-24 12:12:20 +0200

    INOA-437 Load all thingtypes with measurands and configurations

--- service/src/main/java/io/inoa/measurement/things/domain/ThingTypeRepository.java
+++ service/src/main/java/io/inoa/measurement/things/domain/ThingTypeRepository.java
@@ -12,6 +12,10 @@ public interface ThingTypeRepository extends CrudRepository<ThingType, Long> {
 
 	@Join(value = "measurandTypes", type = Join.Type.FETCH)
 	@Join(value = "thingConfigurations", type = Join.Type.FETCH)
+	List<ThingType> findAll();
+
+	@Join(value = "measurandTypes", type = Join.Type.FETCH)
+	@Join(value = "thingConfigurations", type = Join.Type.FETCH)
 	List<ThingType> findByIdentifier(String identifier);
 
 	@Join(value = "measurandTypes", type = Join.Type.FETCH)
